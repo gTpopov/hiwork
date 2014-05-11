d8e17db945b8da21105f104017ede85101af415b
Greg Popov <greghiworld@icloud.com>
2014-05-12 02:06:25 +0300

    almost prepared registration and login form to code them

--- www/protected/models/LoginForm.php
+++ www/protected/models/LoginForm.php
@@ -7,26 +7,23 @@
  */
 class LoginForm extends CFormModel
 {
-	public $username;
-	public $password;
-	public $rememberMe;
+	public $user_main_email;
+	public $user_password;
+	public $remember_me;
 
 	private $_identity;
 
 	/**
 	 * Declares the validation rules.
-	 * The rules state that username and password are required,
-	 * and password needs to be authenticated.
+	 * The rules state that user_main_email and user_password are required,
+	 * and user_password needs to be authenticated.
 	 */
 	public function rules()
 	{
 		return array(
-			// username and password are required
-			array('username, password', 'required'),
-			// rememberMe needs to be a boolean
-			array('rememberMe', 'boolean'),
-			// password needs to be authenticated
-			array('password', 'authenticate'),
+			array('user_main_email, user_password', 'required'),
+			array('remember_me', 'boolean'),
+			array('user_password', 'authenticate'),
 		);
 	}
 
@@ -35,37 +32,35 @@ class LoginForm extends CFormModel
 	 */
 	public function attributeLabels()
 	{
-		return array(
-			'rememberMe'=>'Remember me next time',
-		);
+		return array( 'remember_me'=>'запомнить меня', );
 	}
 
 	/**
-	 * Authenticates the password.
+	 * Authenticates the user_password.
 	 * This is the 'authenticate' validator as declared in rules().
 	 */
 	public function authenticate($attribute,$params)
 	{
 		if(!$this->hasErrors())
 		{
-			$this->_identity=new UserIdentity($this->username,$this->password);
+			$this->_identity = new UserIdentity($this->user_main_email,$this->user_password);
 			if(!$this->_identity->authenticate())
-				$this->addError('password','Incorrect username or password.');
+				$this->addError('user_password','Неправильная пара email/пароль.');
 		}
 	}
 
 	/**
-	 * Logs in the user using the given username and password in the model.
+	 * Logs in the user using the given user_main_email and user_password in the model.
 	 * @return boolean whether login is successful
 	 */
 	public function login()
 	{
-		if($this->_identity===null)
+		if($this->_identity === null)
 		{
-			$this->_identity=new UserIdentity($this->username,$this->password);
+			$this->_identity = new UserIdentity($this->user_main_email,$this->user_password);
 			$this->_identity->authenticate();
 		}
-		if($this->_identity->errorCode===UserIdentity::ERROR_NONE)
+		if($this->_identity->errorCode === UserIdentity::ERROR_NONE)
 		{
 			$duration=$this->rememberMe ? 3600*24*30 : 0; // 30 days
 			Yii::app()->user->login($this->_identity,$duration);
--- www/protected/views/registration/index.php
+++ www/protected/views/registration/index.php
@@ -8,8 +8,12 @@
 
 ?>
 
-<div class="form col-sm-4 col-sm-offset-4">
+<div class="col-sm-6">
 
+</div>
+
+<div class="form col-sm-5">
+    <h2 class="text-center">Присоединяйся!</h2>
     <?php $form=$this->beginWidget('CActiveForm', array(
         'id'=>'users-index-form',
         // Please note: When you enable ajax validation, make sure the corresponding
@@ -24,7 +28,7 @@
             'class' => 'formLabel'
         )); ?>
         <?php echo $form->textField($model,'user_nick_name',array(
-            'placeholder' => 'name@example.com'
+            'placeholder' => 'name-email@example.com'
         )); ?>
         <?php echo $form->error($model,'user_nick_name'); ?>
     </div>
@@ -34,7 +38,7 @@
             'class' => 'formLabel'
         )); ?>
         <?php echo $form->textField($model,'user_main_email',array(
-            'placeholder' => 'myNickName'
+            'placeholder' => 'Ваш никнейм'
         )); ?>
         <?php echo $form->error($model,'user_main_email'); ?>
     </div>
@@ -44,14 +48,17 @@
             'class' => 'formLabel'
         )); ?>
         <?php echo $form->passwordField($model,'user_password',array(
-            'placeholder' => '12345678'
+            'placeholder' => 'Придумайте пароль'
         )); ?>
         <?php echo $form->error($model,'user_password'); ?>
     </div>
 
 
     <div class="col-sm-12 buttons">
-        <?php echo CHtml::submitButton('Submit'); ?>
+        <a class="pull-left" id="reparePass" href="#">Забыл пароль</a>
+        <?php echo CHtml::submitButton('Присоединиться',array(
+            'class' => 'btn btn-info pull-right'
+        )); ?>
     </div>
 
     <?php $this->endWidget(); ?>
