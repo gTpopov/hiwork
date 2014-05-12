3d697caa817051d9064fa04839536ec5ba709470
Alex <alex@gmail.com>
2014-05-12 13:02:01 +0400

    alex-76 12.05.2014

--- www/protected/controllers/RegistrationController.php
+++ www/protected/controllers/RegistrationController.php
@@ -6,6 +6,11 @@
 
             $model = new Users();
 
+            /*$result = Users::model()->findAllBySql('SELECT * FROM hiwork_users');
+            foreach($result as $value) {
+                print $value->user_main_email;
+            }*/
+
             $this->render('index',array(
                 'model' => $model
             ));
