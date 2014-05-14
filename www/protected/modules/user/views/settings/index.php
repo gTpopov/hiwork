c12c1255d76ae8dee43e31bb77fb92c018c83f84
Greg Popov <greghiworld@icloud.com>
2014-05-14 11:02:05 +0300

    Settings user account

--- www/protected/modules/user/views/settings/index.php
+++ www/protected/modules/user/views/settings/index.php
@@ -2,6 +2,9 @@
     Yii::app()->clientScript->registerCssFile('/css/application/user.application/settings/index.css');
 ?>
 
-<div class="col-sm-12">
+<div class="col-sm-12 first-block">
+    <div class="photo-layout"></div>
+    <div class="col-sm-5 user-info-in-first-block">
 
+    </div>
 </div>
