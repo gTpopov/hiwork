14b477d4a3a8970639ef5cfa97e02a8878130963
Alex <alex@gmail.com>
2014-05-14 13:05:53 +0400

    edit reg controller

--- www/protected/controllers/RegistrationController.php
+++ www/protected/controllers/RegistrationController.php
@@ -25,8 +25,24 @@
                     if($model->save())
                     {
 
-                        $model->user_password = $_POST['Users']['user_password'];
-                        $user_id = Yii::app()->db->lastInsertID;
+                        //$model->user_password = $_POST['Users']['user_password'];
+
+                        $user_id   = (int) Yii::app()->db->lastInsertID;
+                        $password  = (string) trim($_POST['Users']['user_password']);
+                        $key       = (string) md5('HiWork.greg.2014'.$password);
+
+                        //$user_id                = (int) Yii::app()->user->getState('__uu_Id');
+                        //$model->user_password   = (string) $_POST['Users']['user_password_repeat'];
+
+                        $message = '<p>
+                                    <b>'.$_POST['Users']['user_nick_name'].' приветсвуем на HiWork!</b>
+                                    <br> Подтвердите свою регистрацию перейдя по
+                                    <a href="'.Yii::app()->request->hostInfo.'/registration/activation?_ukey='.$key.'&uid='.$user_id.'&__utime='.time().'">
+                                    ссылке
+                                    </a> или скопируйте '.Yii::app()->request->hostInfo.'/registration/activation?_ukey='.$key.'&uid='.$user_id.'&__utime='.time().'
+                                    </p>';
+
+
 
                         // Send message on mail
                         Yii::app()->mailer->From = "test@mail.com";
@@ -34,7 +50,7 @@
                         Yii::app()->mailer->AddAddress("mih_76@mail.ru", 'Имя');
                         Yii::app()->mailer->IsHTML(true);
                         Yii::app()->mailer->Subject = "Поступил новый заказ Delphis";
-                        Yii::app()->mailer->Body = "Message test {$user_id}";
+                        Yii::app()->mailer->Body = $message;
 
                         if(!Yii::app()->mailer->Send()) {
                             Yii::app()->user->setFlash('failed-registration',"Ошибка отправки сообщения ".Yii::app()->mailer->ErrorInfo);
@@ -67,6 +83,58 @@
         }
 
         protected function actionAddUser(){
+
+        }
+
+        /**
+         * Function activation account from mail
+         */
+        public function actionActivation()
+        {
+
+            if(isset($_GET['_ukey']))
+            {
+                $model = new Users();
+                $uid = intval($_GET['uid']);
+                $str = Users::model()->findByPk($uid);
+
+                $key = $str->user_password;
+
+
+                if($key === $_GET['_ukey'])
+                {
+                    $cookieID = new CHttpCookie('__utId',$uid);
+                    Yii::app()->request->cookies['__utId']  = $cookieID;
+
+                    $model_auto_login = new LoginForm;
+                    $model_auto_login->setScenario('classicEnter');
+                    $_POST['LoginForm']['user_email']    = $str->user_email;
+                    $_POST['LoginForm']['user_password'] = $str->user_password;
+
+                    $model_auto_login->attributes = $_POST['LoginForm'];
+
+                    if($model_auto_login->login())
+                    {
+                        unset(Yii::app()->request->cookies['__utId']);
+
+                        if($model->updateByPk($uid,array('user_status'=>'1')))
+                        {
+                            $this->redirect(Yii::app()->user->returnUrl);
+                        }
+                        else {
+
+                            $this->redirect('/');
+                        }
+
+                    }
+                    else {
+                        $this->redirect("/");
+                    }
+                }
+                else {
+                    $this->redirect("/");
+                }
+            }
 
         }
 
