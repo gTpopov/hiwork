c1568ef8341c8a9b98ab35d6cff45c6bda92011b
Greg Popov <greghiworld@icloud.com>
2014-05-14 11:13:30 +0300

    user psp

--- www/protected/modules/user/views/settings/index.php
+++ www/protected/modules/user/views/settings/index.php
@@ -2,9 +2,11 @@
     Yii::app()->clientScript->registerCssFile('/css/application/user.application/settings/index.css');
 ?>
 
-<div class="col-sm-12 first-block">
-    <div class="photo-layout"></div>
-    <div class="col-sm-5 user-info-in-first-block">
-
+    <div class="col-sm-12 first-block">
+        <div class="photo-layout"></div>
+        <div class="col-sm-3 user-info-in-first-block">
+            <div class="user-picture">
+                <img src="/images/app.icons/user-icon.png">
+            </div>
+        </div>
     </div>
-</div>
